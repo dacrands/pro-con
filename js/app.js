67133bd198f5de67382f81c11660f52db29be429
David Crandall <davecrands@gmail.com>
2020-07-03 19:22:49 -0500

    Register service worker

--- js/app.js
+++ js/app.js
@@ -161,3 +161,12 @@ clearListsBtn.onclick = () => {
   initLocalStorage()
   proList.innerHTML = conList.innerHTML = "";
 }
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function() {
+    navigator.serviceWorker
+      .register("/serviceWorker.js")
+      .then(res => console.log("service worker registered"))
+      .catch(err => console.log("service worker not registered", err))
+  })
+}
